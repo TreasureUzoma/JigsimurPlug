5e47c44e9917803350f006550ea0aaf051bdc9f5
Treasure Uzoma <123127790+TreasureUzoma@users.noreply.github.com>
2023-11-26 10:32:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function catalogue(){
+  var targetDiv = document.getElementById("catalogue");
+  // Scroll to the targetDiv's top position
+  window.scrollTo({
+    top: targetDiv.offsetTop,
+    behavior: "smooth" // Smooth scrolling
+  });
+}
+function about(){
+  var targetDiv = document.getElementById("about");
+  // Scroll to the targetDiv's top position
+  window.scrollTo({
+    top: targetDiv.offsetTop,
+    behavior: "smooth" // Smooth scrolling
+  });
+}
+/*function menu(){
+  var targetDiv = document.getElementById("footer");
+  // Scroll to the targetDiv's top position
+  window.scrollTo({
+    top: targetDiv.offsetTop,
+    behavior: "smooth" // Smooth scrolling
+  });
+}
+function menu(){
+  var targetDiv = document.getElementById("footer");
+  // Scroll to the targetDiv's top position
+  window.scrollTo({
+    top: targetDiv.offsetTop,
+    behavior: "smooth" // Smooth scrolling
+  });
+}
+function menu(){
+  var targetDiv = document.getElementById("footer");
+  // Scroll to the targetDiv's top position
+  window.scrollTo({
+    top: targetDiv.offsetTop,
+    behavior: "smooth" // Smooth scrolling
+  });
+}*/
